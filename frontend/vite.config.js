e2c460c8c6d74460a5b25e5a1596bb3b45c4cd61
Pachodev77 <pachodev77@gmail.com>
2025-09-12 20:26:13 -0500

    Ajustar configuración de Vercel para el despliegue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
@@ -17,7 +18,6 @@ export default defineConfig({
       }
     }
   },
-  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
