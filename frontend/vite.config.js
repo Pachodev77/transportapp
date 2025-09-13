58ab79634895fdb89018a65c6c995081fd933e0b
Pachodev77 <pachodev77@gmail.com>
2025-09-12 20:34:35 -0500

    Corregir error 404 en dispositivos móviles

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
+  // Configuración para asegurar que las rutas funcionen correctamente en producción
   base: '/',
   plugins: [react()],
   server: {
